23f6c77fc7099b5c822606c49cb1b70f5cc278a7
alanacg <81997080+alanacg@users.noreply.github.com>
2024-02-14 11:53:37 -0500

    Update config.js

--- config.js
+++ config.js
@@ -21,13 +21,13 @@ var config = {
             hidden: false,
             title: 'Display Title',
             image: 'images/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            description: '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident </p>',
             //text for description is html- can rely on that for extra formatting
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: 0
+                center: [-75.74523, 41.45817], //longitude, latitude
+                zoom: 8.16,
+                pitch: 45.00,
+                bearing: -28.80
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -108,6 +108,40 @@ var config = {
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
-        }
-    ]
+        },
+        {
+            id: 'fifth-chapter',
+            alignment: 'left', //options are left, right, center, full
+            hidden: false,
+            title: 'Added Chapter',
+            image: 'images/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            //text for description is html- can rely on that for extra formatting
+            location: {
+                center: [-122.418398, 37.759483],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '', //add additional js function for this key
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+    //add new chapters ahead of this line
+    
+   //don't edit after this line
+        ] 
 };
